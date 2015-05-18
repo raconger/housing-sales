2cbdb409b474914c195df8c6e28397a144630120
raconger <raconger@gmail.com>
2015-05-17 22:55:49 -0700

    Close out after successful publish to chin-ups.io
    Living on https://raconger.shinyapps.io/housing-sales/

--- server.R
+++ server.R
@@ -1,15 +1,29 @@
 library(shiny)
+
+data <- read.csv("data/sales_data.csv")
+data$'Date' <- as.Date(as.character(data$'Date'), "%Y%m%d")
+dfr <- data.frame(data$'Date', data$Buildings.Sq..Ft., data$'Adjusted.Sale.Price')
+dfr[dfr == 0] <- NA
+newdfr <- na.omit(dfr)
+
 shinyServer( 
         function(input, output) {
                 output$enddate <- renderPrint({input$date})
                 output$startdate <- renderPrint({input$date - input$years*365})
                 output$newScatter <- renderPlot({
-                        data <- read.csv("sales_data.csv")
-                        data$'Date' <- as.Date(as.character(data$'Date'), "%Y%m%d")
-                        dfr <- data.frame(data$'Date', data$Buildings.Sq..Ft., data$'Adjusted.Sale.Price')
-                        dfr[dfr == 0] <- NA
-                        newdfr <- na.omit(dfr)
                         with(newdfr[newdfr$data.Date > {input$date - input$years*365},], plot(data.Buildings.Sq..Ft., data.Adjusted.Sale.Price, xlim = range(0:{input$sqft}), ylim = range(0:{input$price})))   
-                        })
+                })
+                output$multiScatter <- renderPlot({
+                        with(newdfr[newdfr$data.Date > {input$date - input$years*365},], plot(newdfr))
+                })
+                output$timePlot <- renderPlot({
+                        with(newdfr[newdfr$data.Date > {input$date - input$years*365},], plot(data.Date, data.Adjusted.Sale.Price/data.Buildings.Sq..Ft.))
+                })
+                output$table <- renderTable({
+                        with(newdfr[newdfr$data.Date > {input$date - input$years*365},],newdfr)
+                })
+                output$summary <- renderPrint({
+                        summary(newdfr)
+                })
         } 
 )
